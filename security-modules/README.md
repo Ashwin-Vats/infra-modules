50f12be6e6e8fa6592911927a229bfe0eff2dc3f
Ashwin Vats <av18011998@gmail.com>
2026-04-21 20:48:29 +0530

    feat: add security groups module

---
+++ security-modules/README.md
@@ -0,0 +1,6 @@
+# Security Groups Module
+
+Creates:
+
+- Bastion security group (SSH access)
+- Node security group (internal communication)
---
+++ security-modules/main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "bastion" {
+  name        = "${var.env}-bastion-sg"
+  description = "Allow SSH access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-bastion-sg"
+  }
+}
+
+resource "aws_security_group" "nodes" {
+  name        = "${var.env}-nodes-sg"
+  description = "Allow node communication"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow all internal VPC traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-nodes-sg"
+  }
+}
